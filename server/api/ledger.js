8d9fef9a76c9ad55a1f77d0842100c1f37a3d01f
Kush Patel <kushpatel21@gmail.com>
2021-09-23 00:23:53 -0500

    Create API for unit price data
    - /api/ledger/current returns total units issued and total invested to calculate unit price

--- server/api/ledger.js
+++ server/api/ledger.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {Ledger} = require('../db/models')
+const {Sequelize} = require('sequelize')
 
 //new ledger entry
 router.post('/new', async (req, res, next) => {
@@ -8,6 +9,21 @@ router.post('/new', async (req, res, next) => {
     //ledger add
     const {newPur} = await Ledger.create(data)
     res.json(newPur)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Get current unit price and invested amount
+router.get('/current', async (req, res, next) => {
+  try {
+    const current = await Ledger.findAll({
+      attributes: [
+        [Sequelize.fn('sum', Sequelize.col('value')), 'totalInvested'],
+        [Sequelize.fn('sum', Sequelize.col('units')), 'totalUnits']
+      ]
+    })
+    res.json(current)
   } catch (err) {
     next(err)
   }
