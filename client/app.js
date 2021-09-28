70514aca2e47da2bf2e335a29d312b2a86863f32
Kush Patel <kushpatel21@gmail.com>
2021-09-27 22:04:26 -0500

    Fetch all stock data from server
    - Create PurchaseStock component to start beginnings of front end
    - Fetch data via axios.get and save to state var
    - Remove yahoo test

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import PurchaseStock from './PurchaseStock'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <PurchaseStock />
     </div>
   )
 }
--- client/components/test/yahoo.js
+++
@@ -1,3 +0,0 @@
-const yf = require('yahoo-finance-webscraper')
-let a = yf.getSingleStockInfo('AAPL210326C00125000')
-console.log(a)
